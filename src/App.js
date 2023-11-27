fc98fadd0337d1bafb9c4af45b88a6c7b9c10a81
Devin Cook <144957285+DevinCookcs@users.noreply.github.com>
2023-11-27 16:21:50 -0700

    added packed stats in the footer and conditional messages based off item array length

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { useState } from "react";
 export default function App() {
   const [items, setItems] = useState([]);
 
+  //functions changing items array
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
@@ -33,7 +34,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -118,10 +119,31 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  // conditional early return
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Add some items to get started</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list. You've packed X.</em>
+      <em>
+        {/* conditional rendering to change message once everything is packed */}
+        {percentage === 100
+          ? "You've packed everything!  Enjoy your trip!"
+          : `You have ${numItems} ${
+              numItems === 1 ? " item" : " items"
+            } on your list.
+        You've packed ${numPacked}
+        ${numPacked === 1 ? " item" : " items"} (${percentage}%).`}
+      </em>
     </footer>
   );
 }
